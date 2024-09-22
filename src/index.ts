9a0b5cef964f687e71967626f31964845c72c93f
MWRose <mwra2018@mymail.pomona.edu>
2024-09-22 14:06:33 -0400

    added test graphql endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import riotRouter from "./routes/riotRoute";
-import privateRouter from "./routes/private"
+import privateRouter from "./routes/private";
 import { ENVVARS } from "./util/envvars";
+import { createHandler } from 'graphql-http/lib/use/http';
+import schema from './graphql/schema';
 
 const app = express();
 
@@ -15,9 +17,10 @@ app.get("/", (req: Request, res: Response, next: NextFunction): void => {
 
 app.use('/riot', riotRouter);
 app.use('/private', privateRouter);
+app.all('/graphql', createHandler({ schema }));
 
 const PORT = ENVVARS.PORT;
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
-})
+});
